8028308977ea88839087235910229b36c7814d13
Daiki Horiyama <d19921207@gmail.com>
2020-03-23 23:21:21 +0900

    add network resources and rds subnet group

--- main.tf
+++ main.tf
@@ -27,3 +27,160 @@ resource "aws_db_option_group" "example" {
     option_name = "MARIADB_AUDIT_PLUGIN"
   }
 }
+
+resource "aws_db_subnet_group" "example" {
+  name = "example"
+  subnet_ids = [aws_subnet.private_0.id, aws_subnet.private_1.id]
+}
+
+
+# ----------------------------------------
+# N/W
+# ----------------------------------------
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_subnet" "public_0" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "example-1a"
+  }
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "ap-northeast-1c"
+
+  tags = {
+    Name = "example-1c"
+  }
+}
+
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_route" "public" {
+  route_table_id = aws_route_table.public.id
+  gateway_id = aws_internet_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "public_0" {
+  subnet_id = aws_subnet.public_0.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_subnet" "private_0" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.65.0/24"
+  availability_zone = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "example_private_1a"
+  }
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.66.0/24"
+  availability_zone = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "example_private_1c"
+  }
+}
+
+resource "aws_route_table" "private_0" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "example_private_0"
+  }
+}
+
+resource "aws_route_table" "private_1" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "example_private_1"
+  }
+}
+
+resource "aws_route_table_association" "private_0" {
+  subnet_id = aws_subnet.private_0.id
+  route_table_id = aws_route_table.private_0.id
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_1.id
+}
+
+resource "aws_eip" "nat_gateway_0" {
+  vpc = true
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_eip" "nat_gateway_1" {
+  vpc = true
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_nat_gateway" "nat_gateway_0" {
+  allocation_id = aws_eip.nat_gateway_0.id
+  subnet_id = aws_subnet.public_0.id
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_nat_gateway" "nat_gateway_1" {
+  allocation_id = aws_eip.nat_gateway_1.id
+  subnet_id = aws_subnet.public_1.id
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_route" "private_0" {
+  route_table_id = aws_route_table.private_0.id
+  nat_gateway_id = aws_nat_gateway.nat_gateway_0.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route" "private_1" {
+  route_table_id = aws_route_table.private_1.id
+  nat_gateway_id = aws_nat_gateway.nat_gateway_1.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
