7685bab48f4d984cee293a997f18da59597e7982
Daiki Horiyama <d19921207@gmail.com>
2020-03-23 23:00:37 +0900

    mariadb監査プラグイン導入

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
+variable "mysql_version" {
+  type = string
+  default = "5.7"
+}
+
 resource "aws_db_parameter_group" "example" {
   name = "example"
-  family = "mysql5.7"
+  family = "mysql${var.mysql_version}"
 
   parameter {
     name = "character_set_database"
@@ -10,5 +15,15 @@ resource "aws_db_parameter_group" "example" {
   parameter {
     name = "character_set_server"
     value = "utf8mb4"
+  }
+}
+
+resource "aws_db_option_group" "example" {
+  name = "example"
+  engine_name = "mysql"
+  major_engine_version = var.mysql_version
+
+  option {
+    option_name = "MARIADB_AUDIT_PLUGIN"
   }
 }
